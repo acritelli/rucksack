57f44d289958fe5a7ea65506dffed7bd0121b036
Anthony Critelli <critellia@gmail.com>
2021-10-21 22:32:00 -0400

    Add tests

--- test/test_config_parser.py
+++ test/test_config_parser.py
@@ -1,10 +1,30 @@
-from toolbag.config_parser import load_config
+from toolbag.config_parser import *
+from unittest.mock import patch, mock_open
 import unittest
+import yaml
 
+mock_config = """
+---
+system:
+  get-uptime:
+    command: "uptime"
+"""
+
+def mock_glob(file):
+  if file == 'fake/*.yml':
+    return ['file1.yml']
+  if file == 'fake/*.yaml':
+    return ['file2.yaml']
 
 class TestConfigParser(unittest.TestCase):
-  def test_placeholder(self):
-      self.assertEqual(7, 7)
 
-  def test_another(self):
-    self.assertEqual(6,7)
+  @patch('builtins.open', new_callable=mock_open, read_data=mock_config)
+  def test_load_config_from_file(self, mockopen):
+    expected_config = yaml.safe_load(mock_config)
+    config = load_config_from_file('fake')
+    self.assertEqual(config, expected_config)
+
+  @patch('glob.glob', mock_glob)
+  def test_find_config_files_in_directory(self):
+    files = find_config_files_in_directory('fake')
+    self.assertEqual(files, ['file1.yml', 'file2.yaml'])
