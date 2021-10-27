0b17da45b5a3aef4be89f890f5affaf2466621e4
Anthony Critelli <critellia@gmail.com>
2021-10-26 22:33:07 -0400

    Remove dead code from main file

--- toolbag_wip.py
+++ toolbag_wip.py
@@ -17,36 +17,26 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def run_command(command):
-  print(f"Run command called on {command}")
-  return
-
-def render_command():
-  pass
-
 def main():
-  completer_dictionary = {}
 
-config = config_parser.get_config()
+  config = config_parser.get_config()
 
-print(config)
+  print(config)
 
-text = prompt_toolkit.prompt('> ', completer=FuzzyCompleter(ToolbagCompleter(config)))
+  text = prompt_toolkit.prompt('> ', completer=FuzzyCompleter(ToolbagCompleter(config)))
 
-print(text.split())
+  print(text.split())
 
-requested_command = text.split()
-current_dictionary = config
-command_string = None
-command_arguments = {}
+  requested_command = text.split()
+  current_dictionary = config
+  command_string = None
+  command_arguments = {}
 
-print(requested_command)
+  print(requested_command)
 
-command_string = parse_command(requested_command, config)
+  command_string = parse_command(requested_command, config)
 
-
-print(f"The command string is {command_string}")
-# print(command_arguments)
+  print(f"The command string is {command_string}")
 
 if __name__ == '__main__':
   while True:
