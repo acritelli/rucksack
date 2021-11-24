e39872e5e37ad47ace12414631d9384b0add8d47
Anthony Critelli <critellia@gmail.com>
2021-11-23 20:47:22 -0500

    Improve connection tests

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,1 +1,22 @@
+import unittest
+from unittest.mock import patch
 from toolbag.connection import *
+
+class MockConnection():
+  def __init__(self, host):
+    pass
+
+  def open(host):
+    pass
+
+  def run(self, *args, **kwargs):
+    return True
+
+@patch('toolbag.connection.Connection', MockConnection)
+class TestToolbagConnection(unittest.TestCase):
+
+  def test_connection(self):
+    my_connection = ToolBagConnection("www.example.com")
+    command_result = my_connection.execute_command("whoami")
+
+    self.assertEqual(command_result, True)
